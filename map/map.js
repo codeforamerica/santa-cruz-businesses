bbcb8d68e8045159d20068fc5c1b322419b2cde6
Chach Sikes <chachasikes@gmail.com>
2012-02-25 15:51:19 -0800

    Created a mini geoJSON dataset, and a full one with all 5000+ busineses. Note 300 of the businesses were not geocoded because we reached our daily limit for geocoding with Google.

--- map/map.js
+++ map/map.js
@@ -13,5 +13,38 @@ map.add(po.image()
     + "/998/256/{Z}/{X}/{Y}.png")
     .hosts(["a.", "b.", "c.", ""])));
 
+
+map.add(po.geoJson()
+    .url("../data/santa_cruz_businesses.json")
+    .on("load", load)
+    .clip(false)
+    .zoom(14));
+
+
+
 map.add(po.compass()
     .pan("none"));
+    
+    
+    
+function load(e) {
+  var cluster = e.tile.cluster || (e.tile.cluster = kmeans()
+      .iterations(16)
+      .size(64));
+
+  for (var i = 0; i < e.features.length; i++) {
+    cluster.add(e.features[i].data.geometry.coordinates);
+  }
+
+  var tile = e.tile, g = tile.element;
+  while (g.lastChild) g.removeChild(g.lastChild);
+
+  var means = cluster.means();
+  means.sort(function(a, b) { return b.size - a.size; });
+  for (var i = 0; i < means.length; i++) {
+    var mean = means[i], point = g.appendChild(po.svg("circle"));
+    point.setAttribute("cx", mean.x);
+    point.setAttribute("cy", mean.y);
+    point.setAttribute("r", Math.pow(2, tile.zoom - 11) * Math.sqrt(mean.size));
+  }
+}
