76116a0c5052d76936c9588d842ced44088871e3
Michael Lawrence Evans <mevans@stamen.com>
2012-02-25 13:53:23 -0800

    Changing API key; centering around santa cruz

--- map/map.js
+++ map/map.js
@@ -2,12 +2,14 @@ var po = org.polymaps;
 
 var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
+    .center({lat: 36.9741171, lon: -122.0307963})
+    .zoom(12)
     .add(po.interact())
     .add(po.hash());
 
 map.add(po.image()
     .url(po.url("http://{S}tile.cloudmade.com"
-    + "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
+    + "/1a193057ca6040fca68c4ae162bec2da" // http://cloudmade.com/register
     + "/998/256/{Z}/{X}/{Y}.png")
     .hosts(["a.", "b.", "c.", ""])));
 
